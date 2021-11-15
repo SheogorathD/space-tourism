eea8bd8790f1ac20c642b3109a00272f7cb42e41
Aurelion Smol <39001782+SheogorathD@users.noreply.github.com>
2021-11-15 20:06:22 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".gallery").slick({
+    prevArrow:
+      '<img src="images/LArrow.svg" alt="left arrow" class="LArrow" />',
+    nextArrow:
+      '<img src="images/RArrow.svg" alt="right arrow" class="RArrow" />',
+  });
+});
